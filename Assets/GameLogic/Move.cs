1489abea7de0a44ffd8cae994d715265733493a1
Cenaras <mathias_rud@hotmail.com>
2022-10-28 14:51:19 +0200

    Move struct

---
+++ Assets/GameLogic/Move.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Move {
+    public Move(BoardPosition fromPos, BoardPosition toPos) {
+        from = fromPos;
+        to = toPos;
+    }
+
+    public BoardPosition from { get; }
+    public BoardPosition to { get; }
+}
